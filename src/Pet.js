979f433ee1672d9f4351d16055c506e759aea20b
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-06-09 17:44:15 +0100

    refactor: group pet config constants

--- src/Pet.js
+++ src/Pet.js
@@ -1,45 +1,43 @@
 /* eslint-disable func-names */
-
-const AGE_INIT = 0;
-const AGE_INCREMENT = 1;
-
-const MAX_FITNESS = 10;
-const FITNESS_INCREMENT = 4;
-const FITNESS_DECREMENT = 3;
-
-const HUNGER_INIT = 0;
-const HUNGER_INCREMENT = 5;
-const HUNGER_DECREMENT = 3;
-
+const petConfig = {
+  AGE_INIT: 0,
+  AGE_INCREMENT: 1, 
+  MAX_FITNESS: 10,
+  FITNESS_INCREMENT: 4,
+  FITNESS_DECREMENT: 3,
+  HUNGER_INIT: 0,
+  HUNGER_INCREMENT: 5,
+  HUNGER_DECREMENT: 3
+};
 function Pet(name) {
   if (!(typeof name === 'string' || name === undefined)) {
     throw new TypeError('name must be a string or undefined');
   }
   this.name = name;
-  this.age = AGE_INIT;
-  this.hunger = HUNGER_INIT;
-  this.fitness = MAX_FITNESS;
+  this.age = petConfig.AGE_INIT;
+  this.hunger = petConfig.HUNGER_INIT;
+  this.fitness = petConfig.MAX_FITNESS;
 }
 
 Pet.prototype.growUp = function() {
-  this.age += AGE_INCREMENT;
-  this.hunger += HUNGER_INCREMENT;
-  this.fitness -= FITNESS_DECREMENT;
+  this.age += petConfig.AGE_INCREMENT;
+  this.hunger += petConfig.HUNGER_INCREMENT;
+  this.fitness -= petConfig.FITNESS_DECREMENT;
 };
 
 Pet.prototype.walk = function() {
-  if (this.fitness + FITNESS_INCREMENT > MAX_FITNESS) {
-    this.fitness = MAX_FITNESS;
+  if (this.fitness + petConfig.FITNESS_INCREMENT > petConfig.MAX_FITNESS) {
+    this.fitness = petConfig.MAX_FITNESS;
   } else {
-    this.fitness += FITNESS_INCREMENT;
+    this.fitness += petConfig.FITNESS_INCREMENT;
   }
 };
 
 Pet.prototype.feed = function() {
-  if (this.hunger - HUNGER_DECREMENT < HUNGER_INIT) {
-    this.hunger = HUNGER_INIT;
+  if (this.hunger - petConfig.HUNGER_DECREMENT < petConfig.HUNGER_INIT) {
+    this.hunger = petConfig.HUNGER_INIT;
   } else {
-    this.hunger -= HUNGER_DECREMENT;
+    this.hunger -= petConfig.HUNGER_DECREMENT;
   }
 };
 
