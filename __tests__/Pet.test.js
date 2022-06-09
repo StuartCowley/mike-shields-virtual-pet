cd2682634298851846aee122e56975c0ae9a0688
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-06-09 13:43:24 +0100

    Pet constructor only accepts string or undefined as name argument

--- __tests__/Pet.test.js
+++ __tests__/Pet.test.js
@@ -8,4 +8,20 @@ describe('Pet constructor', () => {
   it('returns an instance of Pet', () => {
     expect(new Pet()).toBeInstanceOf(Pet);
   });
+
+  it(`sets name prop equal to the name argument (if provided)`, () => {
+    const name = 'Ollie';
+    const pet = new Pet(name);
+    expect(pet.name).toBe(name);
+  });
+
+  it(`sets name prop that is undefined (if no name argument is provided)`, () => {
+    const pet = new Pet();
+    expect(pet.name).toBeUndefined();
+  });
+
+  it('throws a TypeError if the name argument is not a string', () => {
+    expect(() => new Pet(42)).toThrow(TypeError);
+    expect(() => new Pet(false)).toThrow('name must be a string or undefined');
+  });
 });
--- src/Pet.js
+++ src/Pet.js
@@ -1,3 +1,5 @@
-function Pet() {
-}
+  if (!(typeof name === 'string' || name === undefined)) {
+    throw new TypeError('name must be a string or undefined');
+  }
+  this.name = name;
 module.exports = Pet;
